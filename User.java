e0b9fc409fb5ed5333f25553706dc2a80cd9b96f
Kennadi718 <168859814+Kennadi718@users.noreply.github.com>
2025-06-21 17:18:09 -0500

    Add files via upload

---
+++ User.java
@@ -0,0 +1,17 @@
+package com.geauxtracker.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private int points = 0;
+}
