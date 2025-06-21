2d6f486d01953d91e210b44565b3033f0e130e6b
Kennadi718 <168859814+Kennadi718@users.noreply.github.com>
2025-06-21 17:17:24 -0500

    Add files via upload

---
+++ GeauxTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.geauxtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeauxTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GeauxTrackerApplication.class, args);
+    }
+}
